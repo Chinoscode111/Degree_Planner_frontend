b58fe4c341ecaa249a6b251e1bfdb6121f491831
DevSingal <devsingal2000@gmail.com>
2024-03-04 18:59:12 +0530

    added progress bar, yet to style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PieChart from "../components/Home/PieChart";
 import Navbar from "../components/shared/Navbar";
+import Progress_Bar from "../components/Home/Progress_Bar";
 
 const Home = () => {
     const style = {
         backgroundColor : "blue"
+    }
+
+    const rightStyle = {
+        
+        width:"90%",
+        height:"30vh",
+        backgroundColor : "blanchedalmond",
     }
     return (
         <div className="home-cont">
@@ -14,7 +22,15 @@ const Home = () => {
                     <p>96 units</p>
                     <PieChart />
                 </div>
-                <div className="home--right"></div>
+                <div className="home--right" style={rightStyle}>
+                    <h2>Units per course type</h2>
+                    <p>Required units</p>
+
+                    <Progress_Bar name = "Core" credits = {36} />
+                    <Progress_Bar name = "Major" credits = {42} />
+                    <Progress_Bar name = "Liberal arts" credits = {45} />
+                    <Progress_Bar name = "Elective" credits = {9} />
+                </div>
             </div>
             <div className="home--bottom">
                 <ul>
