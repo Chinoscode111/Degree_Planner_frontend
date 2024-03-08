43e85ba9c99bf48b679f6857b4c80b2ba73f04d2
PanavShah1 <panav.shah@gmail.com>
2024-03-08 15:46:31 +0530

    toggle

--- src/components/DegreeProgress/LoadCourses.js
+++ src/components/DegreeProgress/LoadCourses.js
@@ -2,6 +2,70 @@ import React from "react";
 
 const LoadCourses = () =>{
 
+    const courses = [
+        {
+            group: "MA",
+            courses: [{
+                name: "Linear Algebra",
+                code: "MA110",
+                group: "MA"
+            },
+            {
+                name: "Introduction to Mathematics",
+                code: "MA114",
+                group: "MA"
+            },
+            {
+                name: "History of Mathematics",
+                code: "MA113",
+                group: "MA"
+            },
+            {
+                name: "Calculus",
+                code: "MA105",
+                group: "MA"
+            }]
+        },
+        {
+            group: "PH",
+            courses: [{
+                name: "Classical and Quantum Physics",
+                code: "PH110",
+                group: "PH"
+            },
+            {
+                name: "Physics Lab",
+                code: "PH117",
+                group: "PH"
+            }]
+        }
+        
+    ]
+
+    const [coursesList, setCoursesList] = React.useState(courses.map(group => (
+        {...group, courses: group.courses.map(course => ({...course, toggle: true})), toggle: true}
+    )))
+    console.log("courses list", coursesList)
+
+    function toggle(groupName){
+        console.log("toggle", groupName)
+        setCoursesList(prevCourseList => (
+            prevCourseList.map(group => (
+                group.group==groupName.group?
+                    {...group, toggle: !group.toggle} : group
+            ))
+        ))
+    }
+    
+    React.useEffect(() => {
+        console.log("new course list", coursesList);
+        setDisplayGroups(coursesList.map(group => (
+            createGroup(group)
+        )))
+        console.log("display groups", displayGroups)
+    }, [coursesList]);
+
+
     function createLabel(dict){
         console.log("creating label")
         return(
@@ -12,46 +76,44 @@ const LoadCourses = () =>{
         )
     }
 
-    const courses = [
-        {
-            name: "Linear Algebra",
-            code: "MA110"
-        },
-        {
-            name: "Introduction to Computer Science",
-            code: "CS101"
-        },
-        {
-            name: "Classical and Quantum Physics",
-            code: "PH110"
-        },
-        {
-            name: "Physics Lab",
-            code: "PH117"
-        },
-        {
-            name: "Design",
-            code: "DE109"
-        },
-        {
-            name: "Psychology",
-            code: "HS110"
-        },
-        {
-            name: "Calculus",
-            code: "MA105"
-        },
-        {
-            name: "Makerspace",
-            confirm: "MS101"
+    function createGroup(group){
+        console.log("all group", group)
+        if(group.toggle){
+            return(
+                <>
+                    <button onClick={() => (toggle(group))}>{group.group}</button>
+                    {group.courses.map(course => (
+                    course.toggle ? createLabel(course): <p></p>
+                    ))}
+    
+                </>
+                
+            )
         }
-    ]
+        else{
+            return(
+                <button onClick={() => (toggle(group))}>{group.group}</button>
+            )
+        }
+        
+    }
 
-
-    const [displayCourses, setDisplayCourses] = React.useState(courses.map((course) => (
-        createLabel(course)
+    const [displayGroups, setDisplayGroups] = React.useState(coursesList.map(group => (
+        createGroup(group)
     )))
-    // const [text, setText] = React.useState("")
+
+
+    // const [displayCourses, setDisplayCourses] = React.useState(courses.map((course) => (
+    //     createLabel(course)
+    // )))
+
+
+
+
+
+
+
+
 
     function handleChange(event){
         // setText(event.target.value)
@@ -60,25 +122,30 @@ const LoadCourses = () =>{
         console.log(searchText)
 
 
-        const filteredCourses = courses.filter(course => (
-            course.name.toLowerCase().startsWith(searchText) || course.code.toLowerCase().startsWith(searchText)
+        // const filteredCourses = courses.filter(course => (
+        //     course.name.toLowerCase().startsWith(searchText) || course.code.toLowerCase().startsWith(searchText)
+        // ))
+        const filteredCourses = courses.filter(group => (
+            group.courses.map(course => (
+                course.name.toLowerCase().startsWith(searchText) || course.code.toLowerCase().startsWith(searchText)
+            ))
         ))
 
-        console.log(filteredCourses)
+        // console.log("filtered", filteredCourses)
 
-        const filteredCoursesElements = filteredCourses.map(course => (
-            createLabel(course)
-        ))
+        // const filteredCoursesElements = filteredCourses.map(group => (
+        //     group.courses.map(course => createLabel(course))
+        // ))
 
-        console.log(filteredCoursesElements)
+        // console.log(filteredCoursesElements)
 
-        setDisplayCourses(filteredCoursesElements)
+        // setDisplayCourses(filteredCoursesElements.flat())
 
 
-        // filterCourses.forEach(state => {
-        //     setDisplayCourses()
-        // })
-        console.log(displayCourses)
+        // // filterCourses.forEach(state => {
+        // //     setDisplayCourses()
+        // // })
+        // console.log(displayCourses)
 
         // console.log(filterCourses)
 
@@ -94,7 +161,8 @@ const LoadCourses = () =>{
                 placeholder="Enter course"
                 onChange={handleChange}
             ></input>
-            {displayCourses}
+            {displayGroups}
+            {/* {displayCourses} */}
         </div>
         
     )
