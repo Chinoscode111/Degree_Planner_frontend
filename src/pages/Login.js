c2a60efd0cb5e04574ac2e2c3e18b7b7b835211b
Yog Sonawane <yog.sonawane14@gmail.com>
2024-03-18 22:19:03 +0530

    stylechange

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,50 +3,63 @@ import { useState } from "react";
 // import usePasswordToggle from "../components/ShowPassword/usePasswordToggle";
 import { Link } from "react-router-dom";
 import Home from "./Home";
-import Register from "./Register"
+import Register from "./Register";
 import { MdLock } from "react-icons/md";
 import { GoPerson } from "react-icons/go";
 
-import "../components/shared/login.css"
+import "../../src/login.css";
 
 function LoginForm() {
-    // const [PasswordInputType, ToggleIcon] = usePasswordToggle();
+  // const [PasswordInputType, ToggleIcon] = usePasswordToggle();
   return (
-    
     <div className="Welcome">
       <div className="create">
         <div className="increate">
           <p>Welcome Back</p>
         </div>
       </div>
-  <form>
-      <div className="cover">
-        <div rno>
-        <GoPerson />
-            <input id="first" required type="text" placeholder="Roll No." background-image="./Group.jpg" />
-        </div>
-        <div ps>
-        <MdLock />
-            <input id="second" required type="password" placeholder="Password" background-image="./mdi_password.png"/>
-            {/* <span className="password-toogle-icon">{ToggleIcon}</span> */}
-        </div>
-
-        <div className="centerline">
-          <div>
-            <input id="third" type="checkbox" />
-            <label for="third">Remember me</label>
+      <form>
+        <div className="cover">
+          <div rno>
+            <GoPerson />
+            <input
+              id="first"
+              required
+              type="text"
+              placeholder="Roll No."
+              background-image="./Group.jpg"
+            />
           </div>
-          <a>Forgot Password?</a>
-        </div>
+          <div ps>
+            <MdLock />
+            <input
+              id="second"
+              required
+              type="password"
+              placeholder="Password"
+              background-image="./mdi_password.png"
+            />
+            {/* <span className="password-toogle-icon">{ToggleIcon}</span> */}
+          </div>
 
-        <div className="log">
-           <p>Log in</p> 
-        </div>
+          <div className="centerline">
+            <div>
+              <input id="third" type="checkbox" />
+              <label for="third">Remember me</label>
+            </div>
+            <a>Forgot Password?</a>
+          </div>
 
-        <div className="acc">
-        <Link to={"/register"} element={<Register />}><p>Register</p> </Link>
+          <div className="log">
+            <p>Log in</p>
+          </div>
+
+          <div className="acc">
+            <Link to={"/register"} element={<Register />}>
+              <p>Register</p>{" "}
+            </Link>
+          </div>
         </div>
-      </div>
       </form>
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,62 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login"
-import "../components/shared/login.css"
+import Login from "./Login";
+import "../../src/login.css";
 
 import { MdLock } from "react-icons/md";
 import { GoPerson } from "react-icons/go";
 import { PiDotsNineBold } from "react-icons/pi";
 import { LuGraduationCap } from "react-icons/lu";
 
-
-
 const RegisterForm = () => {
-    return (
-        <div className="Whole">
-            <div className="createacc">
-                <div className="increateacc">
-                    <p>Create Account</p>
-                </div>
-            </div>
-
-            <div className="cover2">
-                <div>
-                <GoPerson />
-                    <input id='roll' type = "text" placeholder = "Roll No." />
-                </div>
-                
-                <div>
-                <MdLock />
-                    <input id='pass' type = "password" placeholder = "Password" />
-                </div>
-
-                <div>
-                <MdLock />
-                    <input id='conpass' type = "password" placeholder = "Confirm Password" />
-                </div>
-
-                <div>
-                <PiDotsNineBold />
-                    <input id='dept' type = "text" placeholder = "Department" />
-                </div>
-
-                <div> 
-                <LuGraduationCap />
-                    <input id='degree' type ="text" placeholder = 'Degree' />
-                </div>
-                <div className="SignUp">
-                    <Link to={"/home"} element={<Home />}><p>Sign up</p> </Link>
-                </div>
-
-                <div className="already">
-                    <p>Already have an account? </p>
-                    <Link to={"/"} element={<Login />}><p>Login</p> </Link>
-                </div>
-
-            </div>
+  return (
+    <div className="Whole">
+      <div className="createacc">
+        <div className="increateacc">
+          <p>Create Account</p>
         </div>
-    )
-}
+      </div>
 
-export default RegisterForm
+      <div className="cover2">
+        <div>
+          <GoPerson />
+          <input id="roll" type="text" placeholder="Roll No." />
+        </div>
+
+        <div>
+          <MdLock />
+          <input id="pass" type="password" placeholder="Password" />
+        </div>
+
+        <div>
+          <MdLock />
+          <input id="conpass" type="password" placeholder="Confirm Password" />
+        </div>
+
+        <div>
+          <PiDotsNineBold />
+          <input id="dept" type="text" placeholder="Department" />
+        </div>
+
+        <div>
+          <LuGraduationCap />
+          <input id="degree" type="text" placeholder="Degree" />
+        </div>
+        <div className="SignUp">
+          <Link to={"/home"} element={<Home />}>
+            <p>Sign up</p>{" "}
+          </Link>
+        </div>
+
+        <div className="already">
+          <p>Already have an account? </p>
+          <Link to={"/"} element={<Login />}>
+            <p>Login</p>{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
