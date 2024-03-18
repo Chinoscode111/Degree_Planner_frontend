f36213e9311fa788a00e82f8562f4829e6eabe33
DevSingal <devsingal2000@gmail.com>
2024-03-18 21:39:52 +0530

    completed dnd

--- src/pages/DegreeProgress.js
+++ src/pages/DegreeProgress.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../components/shared/Navbar";
 import LoadCourses from "../components/DegreeProgress/LoadCourses";
 import SemesterCourses from "../components/DegreeProgress/SemesterCourses"
-import "./degprogress.css"
 
 const DegreeProgress = () => {
     return(
